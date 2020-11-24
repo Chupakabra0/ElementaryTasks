a76e500b4a3fe72fa1ebce68b5684141bb8e585c
Сафиюлин Александр <rootalfa4@gmail.com>
2020-11-24 22:42:57 +0200

    First version of completed task

---
+++ FirstTask/ChessBoard.hpp
@@ -0,0 +1,56 @@
+//
+// Created by Александр Сафиюлин on 24.11.2020.
+//
+
+#ifndef FIRSTTASK_CHESSBOARD_HPP
+#define FIRSTTASK_CHESSBOARD_HPP
+
+#include <ostream>
+
+namespace task {
+    class ChessBoard {
+    public:
+        ChessBoard()                  = delete;
+        ChessBoard(const ChessBoard&) = default;
+        ChessBoard(ChessBoard&&)      = default;
+
+        ChessBoard(unsigned rowsAndColumns) : ChessBoard(rowsAndColumns, rowsAndColumns) {}
+
+        ChessBoard(unsigned rows, unsigned columns) : rows_(rows), columns_(columns) {}
+
+        template<class Type>
+        friend std::basic_ostream<Type> &operator<<(std::basic_ostream<Type>& out, const ChessBoard& chessBoard) {
+            if (chessBoard.rows_ == 0 || chessBoard.columns_ == 0) {
+                return out;
+            }
+
+            auto outStarOrBlink = [](std::basic_ostream<Type>& out, unsigned first, unsigned second, auto predicate) -> std::basic_ostream<Type>& {
+                if (predicate(first, second)) {
+                    return out << '*';
+                }
+                return out << ' ';
+            };
+
+            for (auto i = 0u; i < chessBoard.rows_; ++i) {
+                for (auto j = 0u; j < chessBoard.columns_; ++j) {
+                    if (i % 2 == 0) {
+                        outStarOrBlink(out, j % 2, 0u,  std::equal_to<>());
+                    }
+                    else {
+                        outStarOrBlink(out, j % 2, 0u, std::not_equal_to<>());
+                    }
+                }
+                out << '\n';
+            }
+            return out;
+        }
+
+        ~ChessBoard() = default;
+
+    private:
+        unsigned rows_;
+        unsigned columns_;
+    };
+}
+
+#endif //FIRSTTASK_CHESSBOARD_HPP
---
+++ FirstTask/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "ChessBoard.hpp"
+
+int main(int argc, char* argv[]) {
+    try {
+        int rows, columns;
+        if (argc >= 3) {
+            rows = atoi(argv[1]);
+            columns = atoi(argv[2]);
+        }
+        else {
+            std::cout << "Enter count of rows and columns:" << std::endl;
+            std::cin >> rows >> columns;
+        }
+
+        if (rows < 0 || columns < 0) {
+            throw std::exception("Rows or columns can't be lesser than 0...");
+        }
+
+        task::ChessBoard chessBoard(rows, columns);
+        std::cout << chessBoard << std::endl;
+    }
+    catch (std::exception exception) {
+        std::cerr << exception.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
