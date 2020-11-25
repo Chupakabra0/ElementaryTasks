f51a95d2375a0c67d1bf18e660fd1024bd294c98
Сафиюлин Александр <rootalfa4@gmail.com>
2020-11-25 17:45:23 +0200

    View-model added

---
+++ FirstTask/View.hpp
@@ -0,0 +1,31 @@
+//
+// Created by Александр Сафиюлин on 25.11.2020.
+//
+
+#ifndef FIRSTTASK_VIEW_HPP
+#define FIRSTTASK_VIEW_HPP
+
+#include <ostream>
+
+#include "ChessBoard.hpp"
+
+namespace task {
+    class View {
+    public:
+        View()  = delete;
+        ~View() = default;
+
+        View(const Board& chessBoard) : chessBoard_(chessBoard) {
+
+        }
+
+        void Out() {
+            std::cout << this->chessBoard_ << std::endl;
+        }
+
+    private:
+        Board chessBoard_;
+    };
+}
+
+#endif //FIRSTTASK_TASK_H
