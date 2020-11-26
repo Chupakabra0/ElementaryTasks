27c2b07f66d7993a2902f20b30a9ffd2de835a32
Сафиюлин Александр <rootalfa4@gmail.com>
2020-11-26 22:26:19 +0200

    Added services, deleted exceptions and some refactoring

---
+++ SecondTask/Models/Envelope/Envelope.hpp
@@ -0,0 +1,53 @@
+//
+// Created by Александр Сафиюлин on 25.11.2020.
+//
+
+#ifndef SECONDTASK_ENVELOPE_HPP
+#define SECONDTASK_ENVELOPE_HPP
+
+namespace task::second {
+    class Envelope {
+    public:
+        Envelope() = delete;
+        Envelope(const Envelope &) = default;
+        Envelope(Envelope &&) = default;
+
+        Envelope(double heightAndWidth) : Envelope(heightAndWidth, heightAndWidth) {}
+        Envelope(double height, double width) : height_(height), width_(width) {}
+
+        [[nodiscard]] double GetHeight() const {
+            return this->height_;
+        }
+        void SetHeight(unsigned height) {
+            if (std::abs(this->height_ - height) < std::numeric_limits<double>::epsilon()) {
+                return;
+            }
+            this->height_ = height;
+        }
+
+        [[nodiscard]] double GetWidth() const {
+            return this->width_;
+        }
+        void SetWidth(unsigned width) {
+            if (std::abs(this->width_ - width) < std::numeric_limits<double>::epsilon()) {
+                return;
+            }
+            this->width_ = width;
+        }
+
+        ~Envelope() = default;
+
+    private:
+        double height_;
+        double width_;
+    };
+
+    // TODO: functional object for this
+    inline bool CanPutIn(const Envelope &put, const Envelope &in) {
+        return std::max(put.GetHeight(), put.GetWidth()) <= std::max(in.GetHeight(), in.GetWidth())
+               &&
+               std::min(put.GetWidth(), put.GetWidth()) <= std::min(in.GetHeight(), in.GetWidth());
+    }
+
+}
+#endif //SECONDTASK_ENVELOPE_HPP
--- SecondTask/main.cpp
+++ SecondTask/main.cpp
@@ -2,58 +2,52 @@
 #include <iostream>
 #include <string>
 
-#include "Envelope.hpp"
-#include "View.hpp"
-#include "Helpers.hpp"
+#include "Models/Envelope/Envelope.hpp"
+#include "ViewModels/View.hpp"
+#include "Services/Converter.hpp"
 
 int main(int argc, char* argv[]) {
     std::string flag;
     auto firstLaunch = true;
     do {
-        try {
-            double firstHeight, firstWidth;
-            double secondHeight, secondWidth;
-            if (firstLaunch && argc >= 5) {
-                firstHeight  = task::helpers::ConvertString<double>(argv[1]);
-                firstWidth   = task::helpers::ConvertString<double>(argv[2]);
-                secondHeight = task::helpers::ConvertString<double>(argv[3]);
-                secondWidth  = task::helpers::ConvertString<double>(argv[4]);
-                firstLaunch = false;
-            }
-            else {
-                std::cout << "Creating first envelope." << std::endl << "Enter height:" << std::endl;
-                std::cin >> firstHeight;
-                std::cout << "Enter width:" << std::endl;
-                std::cin >> firstWidth;
+        double firstHeight, firstWidth;
+        double secondHeight, secondWidth;
 
-                std::cout << "Creating second envelope." << std::endl << "Enter height:" << std::endl;
-                std::cin >> secondHeight;
-                std::cout << "Enter width:" << std::endl;
-                std::cin >> secondWidth;
-            }
-
-            if (!std::cin) {
-                throw std::runtime_error("Bad input...");
-            }
-            if (firstHeight < 0.0 || firstWidth < 0.0 || secondHeight < 0.0 || secondWidth < 0.0) {
-                throw std::runtime_error("Heights and widths must be greater than 0...");
-            }
-
-            const task::second::Envelope firstEnvelope(firstHeight, firstWidth);
-            const task::second::Envelope secondEnvelope(secondHeight, secondWidth);
-
-            const auto* view = new task::second::View(firstEnvelope, secondEnvelope);
-            view->Out();
-            delete view;
-
-            std::cout << "Continue? [y/Yes]:";
-            std::cin >> flag;
-            std::transform(flag.begin(), flag.end(), flag.begin(), toupper);
+        // TODO multiple argv choose
+        if (firstLaunch && argc >= 5) {
+            firstHeight  = task::helpers::Converter<double>::ConvertString(argv[1]);
+            firstWidth   = task::helpers::Converter<double>::ConvertString(argv[2]);
+            secondHeight = task::helpers::Converter<double>::ConvertString(argv[3]);
+            secondWidth  = task::helpers::Converter<double>::ConvertString(argv[4]);
+            firstLaunch = false;
         }
-        catch (std::exception &exception) {
-            std::cerr << exception.what() << std::endl;
-            return 1;
+        else {
+            std::cout << "Creating first envelope." << std::endl << "Enter height:" << std::endl;
+            std::cin >> firstHeight;
+            std::cout << "Enter width:" << std::endl;
+            std::cin >> firstWidth;
+
+            std::cout << "Creating second envelope." << std::endl << "Enter height:" << std::endl;
+            std::cin >> secondHeight;
+            std::cout << "Enter width:" << std::endl;
+            std::cin >> secondWidth;
         }
+
+        task::helpers::ErrorHandler::AssertAndExit(!std::cin, "Bad input...");
+        task::helpers::ErrorHandler::AssertAndExit
+            (firstHeight < 0.0 || firstWidth < 0.0 || secondHeight < 0.0 ||
+            secondWidth < 0.0, "Heights and widths must be greater than 0...");
+
+        const task::second::Envelope firstEnvelope(firstHeight, firstWidth);
+        const task::second::Envelope secondEnvelope(secondHeight, secondWidth);
+
+        const auto* view = new task::second::View(firstEnvelope, secondEnvelope);
+        view->Out();
+        delete view;
+
+        std::cout << "Continue? [y/Yes]:";
+        std::cin >> flag;
+        std::transform(flag.begin(), flag.end(), flag.begin(), toupper);
     } while (flag != "Y" || flag != "YES");
     return 0;
 }
