a6ded27b923d508d6dd45700d23598f8e3aae35c
Сафиюлин Александр <rootalfa4@gmail.com>
2020-12-18 00:52:36 +0200

    First tests added

---
+++ SecondTask/Tests/ServicesTests/ErrorTests.cpp
@@ -0,0 +1,78 @@
+//
+// Created by Александр Сафиюлин on 17.12.2020.
+//
+
+#include <gtest/gtest.h>
+#include <Errors/ErrorHandler/ErrorHandler.hpp>
+
+
+// ---------------------- HELPER TEST CLASSES ---------------------------------
+
+struct ErrorHandlerFixture : public testing::Test {
+public:
+	ErrorHandlerFixture()
+		: errorCode(task::helpers::ErrorCode::NO_ERROR),
+		errorHandler(new(std::nothrow) task::helpers::ErrorHandler(this->errorCode))
+	{}
+
+	~ErrorHandlerFixture() = default;
+
+	task::helpers::ErrorCode errorCode;
+	
+	std::unique_ptr<task::helpers::ErrorHandler> errorHandler;
+};
+
+//---------------------------- CTOR TESTS -------------------------------------
+
+TEST(ErrorHandlerTests, CtorTest) {
+	const auto errorCode = task::helpers::ErrorCode::NO_ERROR;
+	
+	const std::unique_ptr<task::helpers::ErrorHandler>
+		errorHandler(new(std::nothrow) task::helpers::ErrorHandler(errorCode));
+
+	ASSERT_NE(nullptr, errorHandler);
+	ASSERT_EQ(errorHandler->GetErrorCode(), errorCode);
+}
+
+//---------------------------- GETTERS TESTS ----------------------------------
+
+TEST_F(ErrorHandlerFixture, ErrorHandlerGetterTest) {
+	ASSERT_EQ(this->errorHandler->GetErrorCode(), this->errorCode);
+}
+
+//-------------------------- SETTERS TESTS -----------------------------------
+
+TEST_F(ErrorHandlerFixture, ErrorHandlerSetterTest) {
+	const task::helpers::ErrorCode valuesToSet[]{
+		task::helpers::ErrorCode::NOT_ENOUGH_ARGS,
+		task::helpers::ErrorCode::NO_ERROR,
+		task::helpers::ErrorCode::ETERNAL_SERVER_ERROR,
+		task::helpers::ErrorCode::FILE_NOT_EXIST,
+		task::helpers::ErrorCode::MEMORY_OUT,
+		task::helpers::ErrorCode::PARSE_FAILED,
+		task::helpers::ErrorCode::VALIDATION_FAILED
+	};
+
+	for (const auto& i : valuesToSet) {
+		const auto temp = *this->errorHandler;
+		this->errorHandler->SetErrorCode(i);
+
+		ASSERT_EQ(i, this->errorHandler->GetErrorCode());
+	}
+}
+
+//------------------------- ASSIGNMENTS TESTS ------------------------------
+
+TEST_F(ErrorHandlerFixture, AssignmentTest) {
+	
+	this->errorHandler->SetErrorCode(task::helpers::ErrorCode::NOT_ENOUGH_ARGS);
+	
+	const auto copy = *this->errorHandler;
+
+	ASSERT_EQ(copy.GetErrorCode(), this->errorHandler->GetErrorCode());
+
+	this->errorHandler->SetErrorCode(task::helpers::ErrorCode::NO_ERROR);
+
+	ASSERT_NE(copy.GetErrorCode(), this->errorHandler->GetErrorCode());
+
+}
